0142d3072c1e92ec7c0411e7b03eab04bde67687
Harin3Bone <harin.thananam@gmail.com>
2021-01-21 22:06:27 +0700

    Config - Setup vuex structure

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import Mutations from "./mutations";
+import Actions from "./actions";
+import Getters from "./getters";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
-  mutations: {},
-  actions: {},
+  getters: Getters,
+  mutations: Mutations,
+  actions: Actions,
   modules: {}
 });
